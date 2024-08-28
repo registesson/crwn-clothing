3c2f8be29b1228746541cb95cd2d9ce891b6a6ba
Régis Tesson <registesson@MacBook-Pro-de-Regis.local>
2024-08-28 16:52:04 +0200

    created sign up component, form input component, button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 const Signin = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
@@ -13,6 +15,7 @@ const Signin = () => {
     <div>
       <h1>Sign In Pages</h1>
       <button onClick={logGoogleUser}> Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
