d8e4d3ec50234d54634525ee22578adb1e5aa961
Régis Tesson <registesson@MacBook-Pro-de-Regis.local>
2024-09-26 12:10:23 +0200

    updating sign out saga and sign in sagas

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
@@ -8,7 +8,6 @@ import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component
 import { selectIsCartOpen } from "../../store/cart/cart.selector";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import { signOutUser } from "../../utils/firebase/firebase.utils";
 import { selectCurrentUser } from "../../store/user/user.selector";
 
 import {
@@ -17,10 +16,13 @@ import {
   NavLinks,
   NavLink,
 } from "./navigation.styles";
+import { signOuStart } from "../../store/user/user.action";
 
 const Navigation = () => {
+  const dispatch = useDispatch();
   const currentUser = useSelector(selectCurrentUser);
   const isCartOpen = useSelector(selectIsCartOpen);
+  const signOutUser = () => dispatch(signOuStart());
   return (
     <Fragment>
       <NavigationContainer>
